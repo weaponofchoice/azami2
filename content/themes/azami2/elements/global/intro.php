b73e51182ecc085e9a0668941cf32fc206c9560b
Luc Awater <lucawater@gmail.com>
2014-05-20 17:34:28 +0200

    retrieved data from intro elements

--- content/themes/azami2/elements/global/intro.php
+++ content/themes/azami2/elements/global/intro.php
@@ -1,41 +1,42 @@
-<div id="intro" class="row" style="background:url('<?php the_sub_field('intro_image'); ?>');">
+<div id="intro" class="row">
 	
-	<?php if( have_rows('gallery_left') ):
-		
-		$images = get_field('gallery');
-		print $images;
-	?>
+	<?php
+	$images = get_sub_field('gallery_left');
+	
+	if( $images ): ?>
 	<div class="gallery gallery_intro gallery_left">
 		<div class="royalSlider rsDefault">
-			<?php while( have_rows('gallery_left') ): the_row(); ?>
-
+			<?php foreach( $images as $image ): ?>
 				<li>
 					<div class="rsContent">
-						<img class="rsImg" src="<?php the_sub_field('image'); ?>" />
+						<img class="rsImg" src="<?php echo $image['url']; ?>" />
 					</div>
 				</li>
-
-			<?php endwhile; ?>
+			<?php endforeach; ?>
 		</div>
 	</div>
 	<?php endif; ?>
 	
-	<?php if( have_rows('gallery_right') ): ?>
+	<?php
+	$images = get_sub_field('gallery_right');
+	
+	if( $images ): ?>
 	<div class="gallery gallery_intro gallery_right">
 		<div class="royalSlider rsDefault">
-			<?php while( have_rows('gallery_right') ): the_row(); ?>
-
+			<?php foreach( $images as $image ): ?>
 				<li>
 					<div class="rsContent">
-						<img class="rsImg" src="<?php the_sub_field('image'); ?>" />
+						<img class="rsImg" src="<?php echo $image['url']; ?>" />
 					</div>
 				</li>
-
-			<?php endwhile; ?>
+			<?php endforeach; ?>
 		</div>
 	</div>
 	<?php endif; ?>
 	
-	<div id="logo_type" class="medium-10 medium-centered large-8 large-centered columns"><img src="<?php the_sub_field('intro_typelogo'); ?>" alt="" /></div>
+	<?php
+	$logo = get_sub_field('logo_type');
+	?>
+	<div id="logo_type" class="medium-10 medium-centered large-8 large-centered columns"><img src="<?php echo $logo['url']; ?>" alt="" /></div>
 	<div id="arrow_scroll"></div>
 </div>
