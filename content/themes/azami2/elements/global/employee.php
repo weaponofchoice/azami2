7d46af88d217bd24cad5fd49b82f9c110fcfac68
Luc Awater <lucawater@gmail.com>
2014-05-20 17:22:23 +0200

    retrieved data from employee element

--- content/themes/azami2/elements/global/employee.php
+++ content/themes/azami2/elements/global/employee.php
@@ -1,5 +1,8 @@
+<?php
+$image = get_sub_field('picture');
+?>
 <div class="employee row">
-	<img class="picture small-20 small-offset-2 medium-9 medium-offset-2 large-9 large-offset-2 columns" src="<?php the_sub_field('picture')['url']; ?>" />
+	<img class="picture small-20 small-offset-2 medium-9 medium-offset-2 large-9 large-offset-2 columns" src="<?php echo $image['url']; ?>" />
 	<article class="small-20 small-offset-2 medium-9 medium-offset-2 large-9 large-offset-2 columns">
 		<h2><?php the_sub_field('name'); ?></h2>
 		<?php the_sub_field('description'); ?>
