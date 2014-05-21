9966d1ea5bf9596d45c9171875bb7178578ce3c0
Luc Awater <lucawater@gmail.com>
2014-05-21 10:40:46 +0200

    rebuild: text

--- content/themes/azami2/elements/text/text.php
+++ content/themes/azami2/elements/text/text.php
@@ -1,6 +1,6 @@
 <div class="text">
 	<div class="row">
-		<article class="small-20 small-offset-2 medium-20 medium-offset-2 large-12 large-offset-6 xlarge-10 xlarge-offset-7 columns">
+		<article>
 			<h2><?php the_sub_field('title'); ?></h2>
 			<?php the_sub_field('text'); ?>
 		</article>
