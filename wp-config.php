6fe2040ba2988c8453432715f482277c7b41d796
Luc Awater <lucawater@gmail.com>
2014-05-23 12:49:58 +0200

    wp-config restored

--- wp-config.php
+++ wp-config.php
@@ -1,8 +1,4 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
-define( 'WP_DEBUG', true);
-
-
 // ===================================================
 // Load database info and local development parameters
 // ===================================================
