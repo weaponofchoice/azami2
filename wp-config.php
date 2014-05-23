c62276b630c3761a73984ad49d756669bebf564c
Luc Awater <lucawater@gmail.com>
2014-05-23 14:45:33 +0200

    wp-config debugging

--- wp-config.php
+++ wp-config.php
@@ -1,4 +1,17 @@
 <?php
+//DEBUGGING
+// Turns WordPress debugging on
+define('WP_DEBUG', true);
+
+// Tells WordPress to log everything to the /wp-content/debug.log file
+define('WP_DEBUG_LOG', true);
+
+// Doesn't force the PHP 'display_errors' variable to be on
+define('WP_DEBUG_DISPLAY', false);
+
+// Hides errors from being displayed on-screen
+@ini_set('display_errors', 0);
+
 // ===================================================
 // Load database info and local development parameters
 // ===================================================
