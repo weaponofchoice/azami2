8571333365b85969a439209e9a14dbcf40607ba3
Luc Awater <lucawater@gmail.com>
2014-05-26 11:19:11 +0200

    responsiveness: touch, text_quote

--- content/themes/azami2/elements/text/text_quote.php
+++ content/themes/azami2/elements/text/text_quote.php
@@ -4,7 +4,7 @@
 			<h3><?php the_sub_field('quote'); ?></h3>
 		</div>
 		
-		<article class="small-20 small-offset-2 medium-20 medium-offset-2 large-12 large-offset-1 xlarge-10 columns">
+		<article class="small-24 small-offset-0 medium-24 medium-offset-0 large-12 large-offset-1 xlarge-10 columns">
 			<h2><?php the_sub_field('title'); ?></h2>
 			<?php the_sub_field('text'); ?>
 			<a class="bold" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('link_text'); ?></a>
