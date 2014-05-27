450e3161bafc6dfd76a03f98b52efa23a89fe033
Luc Awater <lucawater@gmail.com>
2014-05-27 17:29:45 +0200

    gallery restored

--- content/themes/azami2/elements/galleries/gallery.php
+++ content/themes/azami2/elements/galleries/gallery.php
@@ -1,4 +1,4 @@
-<!-- <?php
+<?php
 $images = get_sub_field('images');
 if( $images ): ?>
 <div class="gallery">
@@ -24,31 +24,4 @@ if( $images ): ?>
 		<?php endforeach; ?>
 	</div>
 </div>
-<?php endif; ?> -->
-	
-<div class="gallery">
-	
-	<?php if(have_rows('images')): ?>
-    <div class="royalSlider rsDefault contentSlider rsAutoHeight rsHor">
-		<?php
-			while(have_rows('images')): the_row();
-			
-			// scaled images module
-			$g = get_sub_field('image');
-			$g_url = $g['sizes']['large'];
-			
-			if (is_tablet()){
-				$g_url = $g['sizes']['medium'];
-			}
-			
-			if (is_mobile()){
-				$g_url = $g['sizes']['small'];
-			}
-		?>
-			<div>
-            	<img class="rsImg" src="<?php echo $g_url; ?>" />
-			</div>
-		<?php endwhile; ?>		
-	</div>
 <?php endif; ?>
-</div>
