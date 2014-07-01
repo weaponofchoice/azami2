f50ceb71a83dbbbb062c8a869d00af33b18dc441
Luc Awater <lucawater@gmail.com>
2014-07-01 11:20:35 +0200

    exchanged stickykit for sticky

--- content/themes/azami2/coffee/init-stickykit.coffee
+++
@@ -1,5 +0,0 @@
-if $('.menu').length > 0
-	menu = $('.menu')
-	intro_height = $('#intro').height()
-			
-	menu.stick_in_parent()
--- content/themes/azami2/includes/enqueue-scripts.php
+++ content/themes/azami2/includes/enqueue-scripts.php
@@ -14,14 +14,14 @@ function enqueue_theme_scripts() {
 		wp_enqueue_script( 'royalslider', get_template_directory_uri() . '/js/vendor/royalslider.js', 'jquery', false, true );
 		wp_enqueue_script( 'imagelightbox', get_template_directory_uri() . '/js/vendor/imagelightbox.js', 'jquery', false, true );
 		wp_enqueue_script( 'gmaps', get_template_directory_uri() . '/js/vendor/gmaps.js', 'jquery', false, true );
-		wp_enqueue_script( 'stickykit', get_template_directory_uri() . '/js/vendor/stickykit.js', 'jquery', false, true );
+		wp_enqueue_script( 'sticky', get_template_directory_uri() . '/js/vendor/sticky.js', 'jquery', false, true );
 		wp_enqueue_script( 'scrollto', get_template_directory_uri() . '/js/vendor/scrollto.js', 'jquery', false, true );
 		
 		wp_enqueue_script( 'intro', get_template_directory_uri() . '/js/intro.js', 'royalslider', false, true );
 		wp_enqueue_script( 'init-royalslider', get_template_directory_uri() . '/js/init-royalslider.js', 'royalslider', false, true );
 		wp_enqueue_script( 'init-imagelightbox', get_template_directory_uri() . '/js/init-imagelightbox.js', 'imagelightbox', false, true );
 		wp_enqueue_script( 'init-gmaps', get_template_directory_uri() . '/js/init-gmaps.js', 'gmaps', false, true );
-		wp_enqueue_script( 'init-stickykit', get_template_directory_uri() . '/js/init-stickykit.js', 'stickykit', false, true );
+		wp_enqueue_script( 'init-sticky', get_template_directory_uri() . '/js/init-sticky.js', 'sticky', false, true );
 		
 		wp_enqueue_script( 'home', get_template_directory_uri() . '/js/home.js', 'jquery', false, true );
 		wp_enqueue_script( 'scroll', get_template_directory_uri() . '/js/scroll.js', 'scrollto', false, true );
