fe7cc0b110ba8e799337a2fddf9902554e7f6782
Luc Awater <lucawater@gmail.com>
2014-07-02 13:58:33 +0200

    debugging js files on server side

--- content/themes/azami2/includes/enqueue-scripts.php
+++ content/themes/azami2/includes/enqueue-scripts.php
@@ -29,7 +29,25 @@ function enqueue_theme_scripts() {
 		wp_enqueue_script( 'gallery-content', get_template_directory_uri() . '/js/gallery-content.js', 'jquery', false, true );
  	else:
 		// All concatenated and compressed JS in one file:
-		wp_enqueue_script( 'application', get_template_directory_uri() . '/js/app.min.js', array('jquery'), false, true	);
+		wp_enqueue_script( 'height', get_template_directory_uri() . '/js/height.js', 'jquery', false, true );
+		wp_enqueue_script( 'pageload', get_template_directory_uri() . '/js/pageload.js', 'jquery', false, true );
+		
+		wp_enqueue_script( 'royalslider', get_template_directory_uri() . '/js/vendor/royalslider.js', 'jquery', false, true );
+		wp_enqueue_script( 'imagelightbox', get_template_directory_uri() . '/js/vendor/imagelightbox.js', 'jquery', false, true );
+		wp_enqueue_script( 'gmaps', get_template_directory_uri() . '/js/vendor/gmaps.js', 'jquery', false, true );
+		wp_enqueue_script( 'sticky', get_template_directory_uri() . '/js/vendor/sticky.js', 'jquery', false, true );
+		wp_enqueue_script( 'scrollto', get_template_directory_uri() . '/js/vendor/scrollto.js', 'jquery', false, true );
+		
+		wp_enqueue_script( 'intro', get_template_directory_uri() . '/js/intro.js', 'royalslider', false, true );
+		wp_enqueue_script( 'init-royalslider', get_template_directory_uri() . '/js/init-royalslider.js', 'royalslider', false, true );
+		wp_enqueue_script( 'init-imagelightbox', get_template_directory_uri() . '/js/init-imagelightbox.js', 'imagelightbox', false, true );
+		wp_enqueue_script( 'init-gmaps', get_template_directory_uri() . '/js/init-gmaps.js', 'gmaps', false, true );
+		wp_enqueue_script( 'init-sticky', get_template_directory_uri() . '/js/init-sticky.js', 'sticky', false, true );
+		
+		wp_enqueue_script( 'home', get_template_directory_uri() . '/js/home.js', 'jquery', false, true );
+		wp_enqueue_script( 'scroll', get_template_directory_uri() . '/js/scroll.js', 'scrollto', false, true );
+		wp_enqueue_script( 'menu_small', get_template_directory_uri() . '/js/menu_small.js', 'jquery', false, true );
+		wp_enqueue_script( 'gallery-content', get_template_directory_uri() . '/js/gallery-content.js', 'jquery', false, true );
  	endif;
 }
 
