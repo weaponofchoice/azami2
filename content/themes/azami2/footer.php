6848c746239349c5fed4ca90404d7b6154eeab41
Luc Awater <lucawater@gmail.com>
2014-07-04 14:36:55 +0200

    added english footer information

--- content/themes/azami2/footer.php
+++ content/themes/azami2/footer.php
@@ -1,4 +1,39 @@
 			<footer id="footer">
+				<?php
+				 $lang = get_locale();
+				 if($lang == 'en'){
+				 ?>
+ 				<div class="row">
+ 					<ul>
+ 						<li class="small-20 small-offset-2 medium-10 medium-offset-2 large-5 large-offset-2 columns">
+ 							<h4 class="uppercase">Business hours</h4>
+ 							<p>Tuesday - Friday : 9.30am - 06.00pm</p>
+ 							<p>Thursday night : 07.00pm - 09.00pm</p>
+ 							<p>Saturday : 9.30am - 05.00pm</p>
+ 							<p>1st Sunday : 01.00pm - 05.00pm</p>
+ 							<a class="underlined" href="<?php home_url(); ?>/info">Exceptions</a>
+ 						</li>
+ 						<li class="small-20 small-offset-2 medium-10 medium-offset-1 large-5 large-offset-2 columns">
+ 							<h4 class="uppercase">Contact</h4>
+ 							<p>Noordeinde 29</p>
+ 							<p>2514 GB Den Haag</p>
+ 							<p class="phonenumber">+31 (0)70 346 78 05</p>
+ 							<a href="mailto:info@azami.nl" class="email"><p>info@azami.nl</p></a>
+ 							<div class="followus">
+ 								<p>Follow us at:</p>
+ 								<a class="icon-social" href="http://www.facebook.com/AzamiTheHague" target="blank"><img src="<?php bloginfo('template_directory'); ?>/img/icons_social/white_facebook.png" /></a>
+ 								<a class="icon-social" href="http://www.instagram.com/azamioptiek" target="blank"><img src="<?php bloginfo('template_directory'); ?>/img/icons_social/white_instagram.png" /></a>
+ 							</div>
+ 						</li>
+ 						<li class="signup small-20 small-offset-2 medium-20 medium-offset-2 large-6 large-offset-2 columns">
+ 							<h4 class="uppercase">Newsletter</h4>
+ 							<?php mc4wp_form(); ?>
+ 						</li>
+ 					</ul>
+ 				</div>
+				 <?php
+			 	} else {
+				 ?>
 				<div class="row">
 					<ul>
 						<li class="small-20 small-offset-2 medium-10 medium-offset-2 large-5 large-offset-2 columns">
@@ -27,6 +62,9 @@
 						</li>
 					</ul>
 				</div>
+				<?php
+				}
+				?>
 			</footer>
 		</div><!-- end of content -->
 		
