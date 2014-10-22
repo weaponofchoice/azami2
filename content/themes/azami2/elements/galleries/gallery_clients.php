d19ee087112dc39e4369ae221133e2812bb105e6
Luc Awater <lucawater@gmail.com>
2014-10-22 12:10:20 +0200

    modified client gallery url

--- content/themes/azami2/elements/galleries/gallery_clients.php
+++ content/themes/azami2/elements/galleries/gallery_clients.php
@@ -1,6 +1,6 @@
 
 <div class="alpines">
-  <?php echo do_shortcode('[alpine-phototile-for-tumblr src="user" uid="http://azami-glasses.tumblr.com/" imgl="tumblr" style="vertical" size="500" num="8" align="center" max="100"]'); ?>
+  <?php echo do_shortcode('[alpine-phototile-for-tumblr src="user" uid="http://azamioptiek.tumblr.com/" imgl="tumblr" style="vertical" size="500" num="8" align="center" max="100"]'); ?>
 </div>
 
 
