eea05905099701e44427a084effbf9d4f8cc445e
Luc Awater <lucawater@gmail.com>
2014-10-29 11:33:39 +0100

    link client gallery to tumblr page

--- content/themes/azami2/elements/galleries/gallery_clients.php
+++ content/themes/azami2/elements/galleries/gallery_clients.php
@@ -1,6 +1,6 @@
 
 <div class="alpines">
-  <?php echo do_shortcode('[alpine-phototile-for-tumblr src="user" uid="http://azamioptiek.tumblr.com/" imgl="tumblr" style="vertical" size="500" num="8" align="center" max="100"]'); ?>
+  <?php echo do_shortcode('[alpine-phototile-for-tumblr src="custom" tumblrurl="http://azamioptiek.tumblr.com/" imgl="link" curl="http://azamioptiek.tumblr.com/" style="vertical" size="500" num="8" align="center" max="100" nocredit="1"]'); ?>
 </div>
 
 
