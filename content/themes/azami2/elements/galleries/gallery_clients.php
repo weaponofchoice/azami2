f1ff3a29c559d1fbe5e6e710ce7d4a7dc0adc06e
Luc Awater <lucawater@gmail.com>
2014-12-09 17:12:17 +0100

    placed target blank linkover client gallery

--- content/themes/azami2/elements/galleries/gallery_clients.php
+++ content/themes/azami2/elements/galleries/gallery_clients.php
@@ -1,6 +1,8 @@
 
 <div class="alpines">
-  <?php echo do_shortcode('[alpine-phototile-for-tumblr src="custom" tumblrurl="http://azamioptiek.tumblr.com/" imgl="link" curl="http://azamioptiek.tumblr.com/" style="vertical" size="500" num="8" align="center" max="100" nocredit="1"]'); ?>
+  <a href="http://azamioptiek.tumblr.com/" target="_blank">
+    <?php echo do_shortcode('[alpine-phototile-for-tumblr src="custom" tumblrurl="http://azamioptiek.tumblr.com/" imgl="link" curl="http://azamioptiek.tumblr.com/" style="vertical" size="500" num="8" align="center" max="100" nocredit="1"]'); ?>
+  </a>
 </div>
 
 
